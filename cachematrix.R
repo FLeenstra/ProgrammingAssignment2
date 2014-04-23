2297e7a454caea8b8bf1c7bdafac572f3589e9f1
Folkert Leenstra <mail@folkertleenstra.nl>
2014-04-23 20:07:09 +0200

    My first try, I edited the Vector code in order to make it work ... although it feels a bit unsatisfactory just editing mr Peng's code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMartix -->  function is to create an inverted matrix
+##                      Only square matrices are inverted, all other matrices will return an error
+##                      No matrix is returned, instead it is put into cache and can be retrieved by cacheSole()
+##
+## cacheSolve -->       Returns a matrix from cache, absolutely to computation is done in this function
 
-## Write a short comment describing this function
 
+## 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        ## firstly make sure the matrix is square!
+        if (nrow(x) != ncol(x)) { 
+                stop("only square matrices can be inverted") 
+        }
+        
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
@@ -12,4 +31,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
