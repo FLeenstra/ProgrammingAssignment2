ae641fb3ec68e79c6e6c155dd7fdfd18ee655022
Folkert Leenstra <mail@folkertleenstra.nl>
2014-04-23 20:24:37 +0200

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -13,29 +13,44 @@ makeCacheMatrix <- function(x = matrix()) {
                 stop("only square matrices can be inverted") 
         }
         
+        ## creating m as the cache variable
         m <- NULL
+        
+        ## a set function, it sets the origional data
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        
+        ## a get function, to retrieve the origional data
         get <- function() x
+        
+        ## a setsolve function to set the inverted data
         setsolve <- function(solve) m <<- solve
+        
+        ## a getsolve data to get the inverted data
         getsolve <- function() m
+        
+        ## saving the bunch in a list
         list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## a function for retrieving information from makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## first attempt to find the information in m
         m <- x$getsolve()
+        
+        ## if found retrieve from cache
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+        
+        ## if not found, calculate the invertion and save it. 
         data <- x$get()
         m <- solve(data, ...)
         x$setsolve(m)
